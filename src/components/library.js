97a6f6164dabb1b891a415e83934f284144bee7e
FuadMannan <fuad.mannan@gmail.com>
2024-07-27 14:13:53 -0400

    Add pagination to library component

--- src/components/library.js
+++ src/components/library.js
@@ -1,7 +1,7 @@
 import { Navbar } from './navbar';
 import { Table, Pagination } from 'react-bootstrap';
 import { AuthContext } from '../App';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useLayoutEffect, useState } from 'react';
 
 function convertMilliseconds(ms) {
   const minutes = Math.floor(ms / 60000);
@@ -11,20 +11,91 @@ function convertMilliseconds(ms) {
 
 export function Library() {
   const { library, libraryTotal } = useContext(AuthContext);
-
   const [page, setPage] = useState(1);
+  const [pageRange, setPageRange] = useState([]);
   const [songsOnPage, setSongsOnPage] = useState(null);
+  const [totalPaginationItems, setTotalPaginationItems] = useState(1);
+  const [paginationItems, setPaginationItems] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
 
-  const getSongSelection = () =>{
+  const getSongSelection = () => {
     if (library.length > 0) {
       const start = (page - 1) * BATCH_SIZE;
       const end = page * BATCH_SIZE;
       const songs = library.slice(start, end);
       setSongsOnPage(songs);
     }
-  }
+  };
 
   const BATCH_SIZE = 50;
+  const PAGINATION_ITEM_WIDTH = 51;
+
+  useEffect(() => {
+    const newTotal = Math.ceil(libraryTotal.current / BATCH_SIZE);
+    setTotalPages(newTotal);
+  }, [libraryTotal]);
+
+  useLayoutEffect(() => {
+    const table = document.getElementsByTagName('table')[0];
+    const tableWidth = table.offsetWidth;
+    const num = Math.floor(tableWidth / PAGINATION_ITEM_WIDTH);
+    let newRange;
+    if (totalPages + 2 <= num) {
+      newRange = [2, totalPages];
+    } else {
+      newRange = [2, num - 4];
+    }
+    setPageRange(newRange);
+    setTotalPaginationItems(num);
+  }, [totalPages]);
+
+  useEffect(() => {
+    const tempIndices = [];
+    tempIndices.push(
+      <Pagination.Prev
+        disabled={page === 1 ? true : false}
+        onClick={() => setPage((x) => x - 1)}
+      />,
+      <Pagination.Item key={1} active={page === 1} onClick={() => setPage(1)}>
+        1
+      </Pagination.Item>
+    );
+    if (pageRange[0] > 2) {
+      tempIndices.push(<Pagination.Ellipsis />);
+    }
+    for (let index = pageRange[0]; index <= pageRange[1]; index++) {
+      tempIndices.push(
+        <Pagination.Item
+          key={index}
+          active={index === page}
+          onClick={() => setPage(index)}
+        >
+          {index}
+        </Pagination.Item>
+      );
+    }
+    if (pageRange[1] < totalPages - 1) {
+      tempIndices.push(<Pagination.Ellipsis />);
+    }
+    if (totalPages > 1) {
+      tempIndices.push(
+        <Pagination.Item
+          key={totalPages}
+          active={page === totalPages}
+          onClick={() => setPage(totalPages)}
+        >
+          {totalPages}
+        </Pagination.Item>
+      );
+    }
+    tempIndices.push(
+      <Pagination.Next
+        disabled={page === totalPages}
+        onClick={() => setPage((x) => x + 1)}
+      />
+    );
+    setPaginationItems(tempIndices);
+  }, [totalPaginationItems, pageRange, page, totalPages]);
 
   useEffect(getSongSelection, [page, library]);
 
@@ -36,7 +107,7 @@ export function Library() {
         className="d-flex flex-column align-items-center justify-content-center p-5"
         style={{ height: '90vh' }}
       >
-        <div id='tableContainer' style={{ overflowY: 'auto', width: '100%' }}>
+        <div id="tableContainer" style={{ overflowY: 'auto', width: '100%' }}>
           <Table striped bordered hover style={{ marginBottom: '0px' }}>
             <thead>
               <tr>
@@ -78,6 +149,7 @@ export function Library() {
             </tbody>
           </Table>
         </div>
+        <Pagination>{paginationItems.map((x) => x)}</Pagination>
       </div>
     </>
   );
