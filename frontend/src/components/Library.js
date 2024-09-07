d6caada8b704d8b0e1a3815740c7d668930afbab
FuadMannan <fuad.mannan@gmail.com>
2024-09-07 13:48:26 -0400

    Update millisecond conversion to display hours

--- frontend/src/components/Library.js
+++ frontend/src/components/Library.js
@@ -16,9 +16,14 @@ import { getShadowEntries } from '../util/queries';
 import { useLocation } from 'react-router-dom';
 
 function convertMilliseconds(ms) {
-  const minutes = Math.floor(ms / 60000);
+  const hours = Math.floor(ms / 3600000);
+  const minutes = Math.floor(ms / 60000) % 60;
   const seconds = Math.floor((ms % 60000) / 1000);
-  return `${minutes}:${String(seconds).padStart(2, '0')}`;
+  return (
+    `${hours > 0 ? hours + ':' : ''}` +
+    `${String(minutes).padStart(2, '0')}:` +
+    `${String(seconds).padStart(2, '0')}`
+  );
 }
 
 export const modes = ['tracks', 'shadowTracks', 'albums'];
